b989e70a3cde6381744b6d29199271f93db91532
MinkyungKim03 <162525444+MinkyungKim03@users.noreply.github.com>
2024-04-29 21:39:29 +0900

    Update app.js

--- code/app.js
+++ code/app.js
@@ -2,21 +2,27 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// Express에 EJS를 사용하기 위한 설정
+app.set('view engine', 'ejs');
+app.set('views', 'views'); // views 디렉토리 설정
+
 // 사용자 데이터를 저장하기 위한 가상의 데이터베이스
 let users = [];
 
+// 정적 파일을 제공하기 위해 static 미들웨어를 사용
 app.use(express.static('public'));
 
+// POST 요청의 body를 파싱하기 위한 미들웨어를 추가
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // 로그인 페이지로 이동하는 라우트
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
+  res.render('login', { message: null });
 });
 
 // 회원가입 페이지로 이동하는 라우트
 app.get('/join', (req, res) => {
-  res.sendFile(__dirname + '/public/join.html');
+  res.render('join');
 });
 
 // 로그인 요청 처리하는 라우트
@@ -26,7 +32,7 @@ app.post('/login', (req, res) => {
   if (user) {
     res.send(`로그인 성공! 환영합니다, ${userid}님`);
   } else {
-    res.send('아이디 또는 비밀번호가 올바르지 않습니다.');
+    res.render('login', { message: '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.' });
   }
 });
 
@@ -40,6 +46,5 @@ app.post('/join', (req, res) => {
 
 // 서버를 3000 포트에서 시작
 app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중입니다.');
+  console.log('서버가 3000번 포트에서 실행 중 . . .');
 });
-

1f1de6eeb1879d6db2342930d966af6f4f2cd4de
MinkyungKim03 <162525444+MinkyungKim03@users.noreply.github.com>
2024-04-30 11:31:27 +0900

    Update app.js

--- code/app.js
+++ code/app.js
@@ -1,29 +1,28 @@
-const path = require('path'); /**추가 */
-
-const express = require('express'); /**추가 */
-const {restart} = require('nodemon'); /**추가 */
-
+const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 
 // Express에 EJS를 사용하기 위한 설정
-app.set('views', path.join(__dirname,'views')); // views 디렉토리 설정
 app.set('view engine', 'ejs');
-/**dirname으로 바꿈 */
+app.set('views', 'views'); // views 디렉토리 설정
 
 // 사용자 데이터를 저장하기 위한 가상의 데이터베이스
 let users = [];
 
-// 정적 파일을 제공하기 위해 static 미들웨어를 사용
+// 정적 파일을 제공하기 위해 static 미들웨어 사용
 app.use(express.static('public'));
 
-// POST 요청의 body를 파싱하기 위한 미들웨어를 추가
-app.use(bodyParser.urlencoded({ extended: false })); /**false로 바꿈 */
+// POST 요청의 body를 파싱하기 위한 미들웨어 추가
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 로그인 페이지로 이동하는 라우트
 app.get('/login', (req, res) => {
-  res.render('login');
+  res.render('login', { message: null });
+});
+
+// 회원가입 페이지로 이동하는 라우트
+app.get('/join', (req, res) => {
+  res.render('join');
 });
 
 // 로그인 요청 처리하는 라우트
@@ -32,25 +31,20 @@ app.post('/login', (req, res) => {
   const user = users.find(u => u.userid === userid && u.userpw === userpw);
   if (user) {
     res.send(`로그인 성공! 환영합니다, ${userid}님`);
-  } 
-  else {
+  } else {
     res.render('login', { message: '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.' });
   }
 });
 
-// 회원가입 페이지로 이동하는 라우트
-app.get('/join', (req, res) => {
-  res.render('join');
-});
-
-
 // 회원가입 요청 처리하는 라우트
 app.post('/join', (req, res) => {
-  const { joinEmail, joinPassword, joinName, joinBirth, joinTelnumber, joinGender } = req.body;
+  const { joinId, joinPassword } = req.body;
   // 가상의 데이터베이스에 새로운 사용자 추가
-  users.push({ userid: joinEmail, userpw: joinPassword, name: joinName, birth: joinBirth, telnumber: joinTelnumber, gender: joinGender });
+  users.push({ userid: joinId, userpw: joinPassword });
   res.send('회원가입이 완료되었습니다!');
 });
 
 // 서버를 3000 포트에서 시작
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('서버가 3000번 포트에서 실행중 . . .');
+});
